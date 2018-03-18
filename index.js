faece65aa8580223ca655990e840bf08771ba80b
Arturo Poumian <apoumians@gmail.com>
2018-03-18 04:29:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
-
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
